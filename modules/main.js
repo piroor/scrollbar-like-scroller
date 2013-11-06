161b11c38578579babd320d317161ed7387966b6
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2013-11-07 03:26:44 +0900

    Implement scrollbar like scroll

--- modules/main.js
+++ modules/main.js
@@ -1,22 +1,110 @@
 load('lib/WindowManager');
+load('lib/prefs');
+
+var PREF_BASE         = 'extensions.scrollbar-like-scroller@piro.sakura.ne.jp.';
+var PREF_DEBUG        = PREF_BASE + 'debug';
+var PREF_AREA_SIZE    = PREF_BASE + 'areaSize';
+var PREF_SCROLL_DELAY = PREF_BASE + 'scrollDelay';
 
 var config = require('lib/config');
-config.setDefault('extensions.scrollbar-like-scroller@piro.sakura.ne.jp.areaSize', 24);
+config.setDefault(PREF_DEBUG,        false);
+config.setDefault(PREF_AREA_SIZE,    48);
+config.setDefault(PREF_SCROLL_DELAY, 50);
 
 Cu.import('resource://gre/modules/Services.jsm');
 
 const TYPE_BROWSER = 'navigator:browser';
 
+function parseTouchEvent(aEvent) {
+	var chrome = WindowManager.getWindow(TYPE_BROWSER);
+	var content = aEvent.originalTarget;
+	content = content.defaultView || content.ownerDocument.defaultView;
+	var touch = aEvent.touches.item(0);
+	var areaSize = prefs.getPref(PREF_AREA_SIZE);
+	var chromeZoom = chrome.QueryInterface(Ci.nsIInterfaceRequestor)
+						.getInterface(Ci.nsIDOMWindowUtils)
+						.screenPixelsPerCSSPixel;
+	var viewport = chrome.BrowserApp.selectedTab.getViewport();
+	var contentZoom = viewport.zoom;
+	var parsed = {
+		zoom    : contentZoom,
+		width   : Math.round(viewport.width),
+		height  : Math.round(viewport.height),
+		eventX  : Math.round(touch.clientX * contentZoom),
+		eventY  : Math.round(touch.clientY * contentZoom)
+	};
+	parsed.rightEdgeTouching = parsed.width - parsed.eventX <= areaSize;
+	parsed.bottomEdgeTouching = parsed.height - parsed.eventY <= areaSize;
+
+	if (prefs.getPref(PREF_DEBUG) && aEvent.type != 'touchmove')
+		chrome.NativeWindow.toast.show(aEvent.type+'\n'+
+			JSON.stringify(parsed).replace(/,/g,',\n'), 'short');
+
+	parsed.chrome = chrome;
+	parsed.content = content;
+	return parsed;
+}
+
+function updateScrollPosition(aParsedTouch) {
+	var window = aParsedTouch.content;
+	var x = window.scrollX;
+	var y = window.scrollY;
+	if (aParsedTouch.bottomEdgeTouching)
+		x = window.scrollMaxX * (aParsedTouch.eventX / aParsedTouch.width);
+	if (aParsedTouch.rightEdgeTouching)
+		y = window.scrollMaxY * (aParsedTouch.eventY / aParsedTouch.height);
+/*
+	Services.obs.notifyObservers(null, 'Gesture:Scroll', JSON.stringify({
+		x : x,
+		y : y
+	}));
+*/
+//	window.scrollTo(x, y);
+	// set scroll position with delay, because the screen is scrolled by Firefox itself.
+	window.setTimeout(function() {
+		window.scrollTo(x, y);
+	}, prefs.getPref(PREF_SCROLL_DELAY));
+}
+
+var handling = false;
+
 function handleTouchStart(aEvent) {
-	WindowManager.getWindow(TYPE_BROWSER).NativeWindow.toast.show('handleTouchStart', 'short');
+	if (aEvent.touches.length > 1)
+		return;
+	var parsed = parseTouchEvent(aEvent);
+	if (!parsed.rightEdgeTouching && !parsed.bottomEdgeTouching)
+		return;
+	handling = true;
+	updateScrollPosition(parsed);
+	aEvent.stopPropagation();
+	aEvent.preventDefault();
 }
 
 function handleTouchEnd(aEvent) {
-	WindowManager.getWindow(TYPE_BROWSER).NativeWindow.toast.show('handleTouchEnd', 'short');
+	if (!handling)
+		return;
+	if (aEvent.touches.length > 1) {
+		handling = false;
+		return;
+	}
+	handling = false;
+	var parsed = parseTouchEvent(aEvent);
+	updateScrollPosition(parsed);
+	aEvent.stopPropagation();
+	aEvent.preventDefault();
 }
 
 function handleTouchMove(aEvent) {
-	WindowManager.getWindow(TYPE_BROWSER).NativeWindow.toast.show('handleTouchMove', 'short');
+	if (!handling)
+		return;
+	if (aEvent.touches.length > 1) {
+		handling = false;
+		return;
+	}
+	var parsed = parseTouchEvent(aEvent);
+	updateScrollPosition(parsed);
+	aEvent.stopPropagation();
+	aEvent.preventDefault();
 }
 
 function handleWindow(aWindow)
@@ -41,5 +129,6 @@ function shutdown()
 		aWindow.removeEventListener('touchmove', handleTouchMove, true);
 	});
 	WindowManager = undefined;
+	prefs = undefined;
 	config = undefined;
 }
