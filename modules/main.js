b827bc5d3f8648dc9d569af8a9414ff6105394b1
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2013-11-07 00:39:54 +0900

    Add codes to handle touch events

--- modules/main.js
+++ modules/main.js
@@ -7,12 +7,27 @@ Cu.import('resource://gre/modules/Services.jsm');
 
 const TYPE_BROWSER = 'navigator:browser';
 
+function handleTouchStart(aEvent) {
+	WindowManager.getWindow(TYPE_BROWSER).NativeWindow.toast.show('handleTouchStart', 'short');
+}
+
+function handleTouchEnd(aEvent) {
+	WindowManager.getWindow(TYPE_BROWSER).NativeWindow.toast.show('handleTouchEnd', 'short');
+}
+
+function handleTouchMove(aEvent) {
+	WindowManager.getWindow(TYPE_BROWSER).NativeWindow.toast.show('handleTouchMove', 'short');
+}
+
 function handleWindow(aWindow)
 {
 	var doc = aWindow.document;
 	if (doc.documentElement.getAttribute('windowtype') != TYPE_BROWSER)
 		return;
 
+	aWindow.addEventListener('touchstart', handleTouchStart, true);
+	aWindow.addEventListener('touchend', handleTouchEnd, true);
+	aWindow.addEventListener('touchmove', handleTouchMove, true);
 }
 
 WindowManager.getWindows(TYPE_BROWSER).forEach(handleWindow);
@@ -21,6 +36,9 @@ WindowManager.addHandler(handleWindow);
 function shutdown()
 {
 	WindowManager.getWindows(TYPE_BROWSER).forEach(function(aWindow) {
+		aWindow.removeEventListener('touchstart', handleTouchStart, true);
+		aWindow.removeEventListener('touchend', handleTouchEnd, true);
+		aWindow.removeEventListener('touchmove', handleTouchMove, true);
 	});
 	WindowManager = undefined;
 	config = undefined;
